7c78d4e5da07ad49cb1973a8b12565d2e39907e9
razakassar <mrazakassar@gmail.com>
2024-03-18 01:31:53 +0500

    wip

--- fromJsontoExcel.js
+++ fromJsontoExcel.js
@@ -7,41 +7,56 @@ function readJSONFromFile(filePath) {
 	return JSON.parse(jsonString);
 }
 
-// Function to write data to Excel
-function writeToExcel(data, excelFilePath, columnTitles) {
-	// Convert JSON to worksheet
-	const worksheet = XLSX.utils.json_to_sheet(data, {
-		header: columnTitles, // Define custom headers
-		skipHeader: true // Skip the header row because we will add it manually
+function writeToExcel({ entities }, excelFilePath, columnTitles) {
+	// Flatten the data objects
+	const flatData = entities.map(({ properties }) => ({
+		name: properties?.name ?? "",
+		phone_number: properties?.phone_number ?? "",
+		linkedin: properties?.linkedin?.value ?? "",
+		facebook: properties?.facebook?.value ?? "",
+		twitter: properties?.twitter?.value ?? "",
+		website: properties?.website?.value ?? "",
+		contact_email: properties?.contact_email ?? "",
+	}));
+
+	// Convert the flat JSON array to a worksheet
+	const worksheet = XLSX.utils.json_to_sheet(flatData, {
+		header: columnTitles,
+		skipHeader: true // We'll add the header manually
 	});
 
-	// Adding custom headers
-	const range = XLSX.utils.decode_range(worksheet['!ref']); // get the range of the sheet
-	for (let C = range.s.c; C <= range.e.c; ++C) {
-		const address = XLSX.utils.encode_col(C) + "1"; // construct A1, B1, C1, ...
-		if (!worksheet[address]) continue; // if cell doesn't exist, move on
-		worksheet[address].v = columnTitles[C]; // assign the title to the cell value
+	// Add the header row manually
+	for (let i = 0; i < columnTitles.length; i++) {
+		const cellRef = XLSX.utils.encode_cell({ c: i, r: 0 }); // Cell reference in A1 format
+		if (!worksheet[cellRef]) {
+			worksheet[cellRef] = {};
+		}
+		worksheet[cellRef].v = columnTitles[i]; // Set the value of the cell to the column title
 	}
 
-	// Create a new workbook
+	// Adjust starting range of the worksheet if not A1
+	if (worksheet['!ref']) {
+		const range = XLSX.utils.decode_range(worksheet['!ref']);
+		range.s.r = 0; // Set start row to 0
+		worksheet['!ref'] = XLSX.utils.encode_range(range);
+	}
+
+	// Create a new workbook and append the worksheet
 	const workbook = XLSX.utils.book_new();
+	XLSX.utils.book_append_sheet(workbook, worksheet, 'Data');
 
-	// Append the worksheet to the workbook
-	XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
-
-	// Write workbook to the excel file
+	// Write the workbook to the file
 	XLSX.writeFile(workbook, excelFilePath);
 }
 
 // File paths
-const jsonFilePath = 'data.json';  // Path to your JSON file
-const excelFilePath = 'output.xlsx'; // Path where you want to save the Excel file
+const jsonFilePath = './response_1710671281478.json';  // Path to your JSON file
+const excelFilePath = 'email.xlsx'; // Path where you want to save the Excel file
 
 // Read data from JSON file
 const jsonData = readJSONFromFile(jsonFilePath);
-
 // Define your column titles order
-const columnTitles = ['Name', 'City', 'Age'];
+const columnTitles = ['name', 'phone_number', 'linkedin', 'facebook', 'twitter', 'website', 'contact_email'];
 
 // Write data to Excel file with custom column titles
 writeToExcel(jsonData, excelFilePath, columnTitles);
